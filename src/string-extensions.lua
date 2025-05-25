5fb008b34d6d5720ba31d61c1f9d53eac8c29a94
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2025-05-25 19:56:43 +0100

    Bump string-extensions version

--- src/string-extensions.lua
+++ src/string-extensions.lua
@@ -1,6 +1,6 @@
 --[[lit-meta
     name = "Richy-Z/string-extensions"
-    version = "0.1.4"
+    version = "0.1.5"
     dependencies = {}
     description = "Small extensions to Lua's default string library"
     tags = { "strings", "split", "regex", "random" }
